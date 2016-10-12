2bafc905504edf099cc7c09811b6a8a824e099ba
Yash Katariya <ykat95@gmail.com>
2016-10-13 01:18:22 +0530

    Update LCS.py

--- Python/LCS.py
+++ Python/LCS.py
@@ -1,26 +1,45 @@
-def lcs(a, b):
-    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
-    # row 0 and column 0 are initialized to 0 already
-    for i, x in enumerate(a):
-        for j, y in enumerate(b):
-            if x == y:
-                lengths[i+1][j+1] = lengths[i][j] + 1
-            else:
-                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
-    # read the substring out from the matrix
-    result = ""
-    x, y = len(a), len(b)
-    while x != 0 and y != 0:
-        if lengths[x][y] == lengths[x-1][y]:
-            x -= 1
-        elif lengths[x][y] == lengths[x][y-1]:
-            y -= 1
-        else:
-            assert a[x-1] == b[y-1]
-            result = a[x-1] + result
-            x -= 1
-            y -= 1
-    return result
-a = input()
-b = input()
-print (lcs(a,b))
+def lcs(x,y):
+	lenx=len(x)
+	leny=len(y)
+
+	# used for DP
+	l=[[None]*(leny+1) for i in range(lenx+1)]
+
+	# Following steps build L[m+1][n+1] in bottom up fashion. Note
+    # that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]
+	for i in range(lenx+1):
+		for j in range(leny+1):
+			if i==0 or j==0:
+				l[i][j]=0
+			elif x[i-1]==y[j-1]:
+				l[i][j]=1+l[i-1][j-1]
+			else:
+				l[i][j]=max(l[i-1][j],l[i][j-1])
+
+	# returns the length
+	print 'The length of LCS is: ',l[lenx][leny]
+
+	# printing the longest common subsequence
+	i=lenx
+	j=leny
+	final=''
+
+	while i>0 and j>0:
+		# If current character in X[] and Y are same, then
+        # current character is part of LCS
+		if x[i-1]==y[j-1]:
+			final+=x[i-1]
+			i-=1
+			j-=1
+		# If not same, then find the larger of two and
+        # go in the direction of larger value
+		elif l[i-1][j]>l[i][j-1]:
+			i-=1
+		else:
+			j-=1
+
+	print 'The LCS is: ',final[::-1]
+
+a = raw_input()
+b = raw_input()
+lcs(a,b)
