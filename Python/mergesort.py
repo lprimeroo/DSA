66d26b2548507048e1e24f31968fca5436dfc487
Yash Katariya <ykat95@gmail.com>
2016-10-14 08:26:33 +0530

    Update mergesort.py

--- Python/mergesort.py
+++ Python/mergesort.py
@@ -1,3 +1,28 @@
+# ALAINA KAFKES IMPLEMENTATION - alainakafkes
+
+## Mergesort implementation in Python
+## Runtime: O(n log(n))
+## Space: O(n)
+## Advantages: guaranteed good complexity (no need to worry about choice of pivot as in quicksort)
+## Disadvantages: more temporary data storage needed than quicksort
+
+#def mergesort(arr):
+#    if len(arr) <= 1:
+#        return arr
+#    midpoint = len(arr) // 2
+#    return merger(mergesort(arr[:mid]), mergesort(arr[mid:]))
+#def merger(arrLeft, arrRight):
+#    if not arrLeft:
+#        return arrRight
+#    if not arrRight:
+#        return arrLeft
+#    if arrLeft[0] < arrRight[0]:
+#        return arrLeft[0] + merger(arrLeft[1:], arrRight)
+#    return arrRight[0] + merger(arrLeft, arrRight[1:])
+
+
+
+# YASH KATARIYA IMPLEMENTATION - yashk2810
 def merge(L,R,A):    # Recursive and stable(for equal numbers it preserves their original position) algorithm
 	nL=len(L)
 	nR=len(R)
