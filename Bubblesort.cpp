d2b2cd55eafddd450b0286fff176d2b9a2e7880c
Deepanshu Rekhi <drekhi12@gmail.com>
2016-10-27 19:21:24 +0530

    Bubblesort.cpp
    Efficient way of writing bubble sort in cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -14,10 +14,11 @@ int main()
 	arrayToSort = new int[arraySize];
 	for(i=0;i<arraySize;i++)
 		cin>>arrayToSort[i];
-
+	//flag checks whether there was a swap or not, if there was no swap then no need to continue to loop, just break out of it
+	int flag=0;
 	//Contains two loops || Note that in both average and worst-case scenarios, results in a big O notation of O(N^2)
 	for(i=0;i<arraySize-1;i++)
-	{
+	{	flag=0;
 		for(j=0;j<arraySize-i-1;j++)
 		{
 			//Compares the values of all elements, swapping the lower figures for higher ones 
@@ -26,8 +27,11 @@ int main()
 				temp = arrayToSort[j];
 				arrayToSort[j] = arrayToSort[j+1];
 				arrayToSort[j+1] = temp;
+				flag=1;
 			}
 		}
+	 if(flag==0)
+		 break;
 	}
 
 	cout<<"Sorted array\n";
